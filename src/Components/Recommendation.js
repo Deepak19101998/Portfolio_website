bb49a06a60a1f1301432739f3c10bdb83c89ad11
Deepak_gupta <dg14964@gmail.com>
2020-11-17 04:02:15 -0500

    Removed the comment part from the Recommendation component and add the RecommendationModal component on Recommendation component

--- src/Components/Recommendation.js
+++ src/Components/Recommendation.js
@@ -84,28 +84,6 @@ class Recommendation extends Component{
                     ))}
                 </div>
                 <RecommendationModal actualId={this.state.actualId} recommendationcards={this.state.recommendationcards}/>
-        
-                    
-
-                {/* <div className="modal fade" id='a'>
-                    <div className="modal-dialog modal-md">
-                        <div className="modal-content">       
-                            <div className="modal-header">
-                                <h3>{recommendationcards[actualId].title}</h3>
-                            </div>
-                            <div className="modal-body">
-                                {recommendationcards[actualId].about}
-                            </div>
-                            <div className="mb-3 modal-footer">
-                                {recommendationcards[actualId].company} 
-                            </div>
-                            <div className="text-center">
-                            <input type="button" value="Close" data-dismiss="modal"
-                                className="btn btn-md btn-danger mb-3" />
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
             </div>
         );
     }
