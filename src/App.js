fc32aa59b7acbdd8b0b18e5f47f9e8e1c1fddbe6
Deepak_gupta <dg14964@gmail.com>
2020-11-17 03:50:08 -0500

    Added the routing on App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from "react";
 import About from "./Components/About.js";
 import Blog from "./Components/Blog.js";
+import Contact from "./Components/Contact.js";
 import Footer from "./Components/Footer.js";
 import Navbar from "./Components/Navbar.js";
 import Project from "./Components/Project.js";
 import Recommendation from "./Components/Recommendation.js";
 import Technology from "./Components/Technology.js";
 import Title from "./Components/Title.js";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import NotFound from "./Components/NotFound.js";
+import ProjectPage from "./Components/ProjectPage.js";
 
 function App(){
   return(
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <Title/>
-      <Recommendation />
-      <Technology />
-      <Project />
-      <About />
-      <Blog />
+        <Switch>
+
+          <Route exact path="/">
+            <Title/>
+            <Recommendation />
+            <Technology />
+            <Project />
+            <About />
+            <Blog />
+          </Route>
+
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/project" component={Project}/>
+          <Route exact path="/projectpage/:id" component={ProjectPage}/>
+          <Route component={NotFound}/>
+
+        </Switch>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
